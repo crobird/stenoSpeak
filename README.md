bf6b84adf310041fa60dac2c2939333c37c8a80d
BJ Murphy <crobird@giftbot.org>
2020-05-12 13:14:10 -0700

    Added line support more officially, independent line/word delays, suffix match

--- README.md
+++ README.md
@@ -1,6 +1,27 @@
 # stenoSpeak
-Script for TTSing words on the mac for use in Steno practice
+Script for TTSing words on the mac for use in Steno practice.
 
-This python scripts assumes a location for your "say" command on macOS. The rest is mostly customizable.
+This python script assumes access to the "say" command, provided on macOS to convert text to speech.
 
-Example: ./stenospeak.py --the_end -v Samantha -r 100 --limit 10
+By default, the system dictionary of words is used. Alternate dictionaries can be used which can be lines of words or sentences.
+
+## Usage
+Type `./stenospeak --help` to see all of the options.
+
+Here are some examples:
+
+##### Use alternate dictionary file of words
+
+	./stenospeak.py -D words.txt
+
+##### Use alternate dictionary file of sentences and no delay between words.
+	
+	./stenospeak.py -D sentences.txt --word_delay 0
+
+##### Include information about hitting the end of the file and some params to change the TTS voice and speech rate
+
+	./stenospeak.py --the_end -v Samantha -r 100 --limit 10
+
+##### Only use entries that have words ending in "tion"
+
+	./stenospeak.py -S tion
--- stenospeak.py
+++ stenospeak.py
@@ -8,7 +8,7 @@ from time import sleep
 
 SAY_CMD = "/usr/bin/say"
 DEFAULT_DICT_FILE = "/usr/share/dict/words"
-DEFAULT_DELAY = 2
+DEFAULT_DELAY = 2.0
 ENDING_PHRASES = [
 	"That's all folks",
 	"Over and out",
@@ -26,42 +26,46 @@ def say(thing, voice=None, rate=None):
 	check_output(args)
 
 
-def get_words(dict_file, randomize, filter_regex=None, min_length=None, max_length=None, limit=None):
-	words = []
+def get_entries(dict_file, randomize, letter_filter_regex=None, suffix_filter_regex=None, min_length=None, max_length=None, limit=None):
+	entries = []
 	with open(dict_file, "r") as fh:
 		for line in fh:
 			l = line.rstrip()
-			if filter_regex and not re.match(filter_regex, l):
+			if letter_filter_regex and not re.match(letter_filter_regex, l):
+				continue
+			if suffix_filter_regex and not re.match(suffix_filter_regex, l):
 				continue
 			if min_length and len(l) < min_length:
 				continue
 			if max_length and len(l) > max_length:
 				continue
-			words.append(l)
+			entries.append(l)
 
 	if randomize:
-		shuffle(words)
+		shuffle(entries)
 
 	if limit:
-		return words[:limit]
+		return entries[:limit]
 
-	return words
-
-
-def get_filter_regex(letter_filter):
-	if not letter_filter:
-		return None
-	return r"^[{}]+$".format(letter_filter)
+	return entries
 
 
 def main(args):
-	filter_regex = get_filter_regex(args.letter_filter)
+	letter_filter_regex = r"^[{}]+$".format(args.letter_filter) if args.letter_filter else None
+	suffix_filter_regex = r".*{}(\W|$)".format(args.suffix_filter) if args.suffix_filter else None
 
-	words = get_words(args.dict_file, not args.in_order, filter_regex, args.min_length, args.max_length, args.limit)
-	for w in words:
-		print(w)
-		say(w, args.voice, args.rate)
-		sleep(args.delay)
+	entries = get_entries(args.dict_file, not args.in_order, letter_filter_regex, suffix_filter_regex, args.min_length, args.max_length, args.limit)
+	for e in entries:
+		print(e)
+		if args.word_delay == 0:
+			say(e, args.voice, args.rate)
+		else:
+			bits = re.split(r"\s+", e)
+			for i, b in enumerate(bits):
+				say(b, args.voice, args.rate)
+				if i < len(bits) - 1:
+					sleep(args.word_delay)
+		sleep(args.line_delay)
 
 	if args.the_end:
 		shuffle(ENDING_PHRASES)
@@ -73,17 +77,26 @@ if __name__ == '__main__':
 
 	parser = argparse.ArgumentParser()
 	parser.add_argument('-D', '--dict_file', help="Dictionary file to use (default: {})".format(DEFAULT_DICT_FILE), default=DEFAULT_DICT_FILE)
-	parser.add_argument('-d', '--delay', default=DEFAULT_DELAY, type=float, help="Seconds of delay between words (default: {})".format(DEFAULT_DELAY))
+	parser.add_argument('-d', '--delay', default=DEFAULT_DELAY, type=float, help="Seconds of delay between items (default: {})".format(DEFAULT_DELAY))
+	parser.add_argument('--line_delay', type=float, help="Delay between lines, defaults to value of --delay")
+	parser.add_argument('--word_delay', type=float, help="Delay between words in a line, defaults to value of --delay")
 	parser.add_argument('-l', '--limit', help="Limit to N words (default is all of them)", type=int)
 	parser.add_argument('--in_order', default=False, help='Play dictionary in order (default is to randomize)', action='store_true')
 	parser.add_argument('-v', '--voice', help="Specify the voice to use (type 'say --voice ?') to see your options")
 	parser.add_argument('-r', '--rate', help="Specify the rate in words per minute (try -r 100)")
 	parser.add_argument('-L', '--letter_filter', help="Only use words that contain the specified letters (eg: -L asfx would use fax but not fox)")
+	parser.add_argument('-S', '--suffix_filter', help="Only use lines that have a word with the suffix (eg: -S tion would use lines with 'action')")
 	parser.add_argument('--max_length', type=int, help="Max length of word")
 	parser.add_argument('--min_length', type=int, help="Min length of word")
 	parser.add_argument('--the_end', default=False, action="store_true", help="Announce the end of the list")
 	parser.add_argument('-I', '--initial_delay', type=int, help="Delay start for N seconds")
 	args = parser.parse_args()
+
+	if args.line_delay == None:
+		args.line_delay = args.delay
+
+	if args.word_delay == None:
+		args.word_delay = args.delay
 
 	if not isfile(SAY_CMD):
 		print("Uh oh, can't find 'say' command at {}".format(SAY_CMD))
